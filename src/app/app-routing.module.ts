23803b50f85528d3d55eacd0c8e7c28b2b0a2dbc
CDevv <83121988+CDevv@users.noreply.github.com>
2021-04-24 15:23:44 +0300

    upload the files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { XyzComponent } from '../app/xyz/xyz.component';
+import { HomeComponent } from '../app/home/home.component';
+
+const routes: Routes = [
+  {path: 'gaming', component: XyzComponent},
+  {path: 'home', component: HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
